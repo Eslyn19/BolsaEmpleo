adc5080a27b297bd5582ebeff8512820cfb39d64
Eslyn19 <andresjara479@gmail.com>
2026-03-06 10:21:16 -0600

    Commit inicial

---
+++ src/main/java/una/ac/cr/p1bolsaempleo/P1BolsaEmpleoApplication.java
@@ -0,0 +1,11 @@
+package una.ac.cr.p1bolsaempleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1BolsaEmpleoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(P1BolsaEmpleoApplication.class, args);
+    }
+}
---
+++ src/test/java/una/ac/cr/p1bolsaempleo/P1BolsaEmpleoApplicationTests.java
@@ -0,0 +1,13 @@
+package una.ac.cr.p1bolsaempleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1BolsaEmpleoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
